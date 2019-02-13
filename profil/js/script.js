309a6bfcd43e962308ef562eab100b4ef1b41487
muhcahyo2 <jokotricahyo129@gmail.com>
2019-02-13 21:53:02 +0700

    add scroll spy fixed

--- profil/js/script.js
+++ profil/js/script.js
@@ -1,30 +1,29 @@
 $(document).ready(function(){
   // $(".content").fadeIn(1000);
   $(".preloader").fadeOut(5000);
-  $('.sidenav').sidenav();
+});
+$(document).ready(function () {
+  $('body').scrollspy({ target: ".navbar", offset: 50 });
+  // Add smooth scrolling to all links
+  $("a").on('click', function (event) {
 
-      $('body').scrollspy({ target: ".navbar", offset: 50 });
-
-    // Add smooth scrolling on all links inside the navbar
-    $("#myNavbar a").on('click', function(event) {
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
       // Prevent default anchor click behavior
       event.preventDefault();
 
-    // Store hash
-    var hash = this.hash;
+      // Store hash
+      var hash = this.hash;
 
-    // Using jQuery's animate() method to add smooth page scroll
-    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
-      scrollTop: $(hash).offset().top
-      }, 800, function(){
+        scrollTop: $(hash).offset().top
+      }, 800, function () {
 
-      // Add hash (#) to URL when done scrolling (default click behavior)
-      window.location.hash = hash;
-    });
-  }  // End if
-});
-
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
 });
